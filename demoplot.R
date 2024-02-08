9b62b1bd5c588a5ab1ac1370e8cbfee36cc3b544
Deepayan Sarkar <deepayan.sarkar@gmail.com>
2024-02-08 23:14:18 +0530

    minor citation updates

--- demoplot.R
+++ demoplot.R
@@ -1,5 +1,6 @@
 
 library(nhanesA)
+nhanesOptions(log.access = TRUE)
 
 demo_all = nhanesSearchTableNames("DEMO")
 demo_all
@@ -16,19 +17,22 @@ demo_combined = lapply(all_demo_data, `[`, common_vars) |> do.call(what = rbind)
 dim(demo_combined)
 
 
-library(ragg)
+## library(ragg)
+
+## agg_png(filename = "images/demoplot.png",
+##         pointsize = 20, res = 96,
+##         width = 12 * 1.2, height = 7 * 1.2, units = "in")
+
+## png(filename = "images/demoplot.png", width = 1200, height = 700)
+
+
+pdf("images/demoplot.pdf", width = 12 * 0.9, height = 7 * 0.85)
 
 
 library("ggplot2")
 
 demo_combined <- transform(demo_combined, cycle = substring(SDDSRVYR, 8, 17))
 ## demo_combined <- transform(demo_combined, cycle = factor(as.numeric(SDDSRVYR)))
-
-agg_png(filename = "images/demoplot.png",
-        pointsize = 20, res = 96,
-        width = 12 * 1.2, height = 7 * 1.2, units = "in")
-
-## png(filename = "images/demoplot.png", width = 1200, height = 700)
 
 demo_combined |> 
     xtabs(~ cycle + RIAGENDR + RIDRETH1, data = _) |>
